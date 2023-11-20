d6d691bae83da309114ece542540f0315568f327
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2023-11-20 18:52:41 -0300

    compiled executables

--- RC-IPv4-to-IPv6/Assets/Scripts/IPPacket.cs
+++ RC-IPv4-to-IPv6/Assets/Scripts/IPPacket.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class IPPacket
 {
